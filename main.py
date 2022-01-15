eaab56bf718316226edd7626599a6bfb7942172e
Isaiah08-D <isaiahgrisha@gmail.com>
2022-01-15 10:56:39 +0100

    Finished (i think) BananaText

--- main.py
+++ main.py
@@ -1,150 +1,91 @@
 from tkinter import *
+from tkinter import font
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from tkinter import filedialog
 import os
-import json
+import json 
 import requests
-from apps import BananaText, SudokuGrid, Shell, Games, Shotput
+from apps import Screen, BananaText
 
 
-
-class Main(Frame):
-    def __init__(self, root, height, width, bg):
-        Frame.__init__(self, root, height=height, width=width, bg=bg)
-
+class Main(Frame): 
+    def __init__(self, root, height, width, color):
+        Frame.__init__(self, root, height=height, width=width, bg=color)
+        
         self.root = root
-        self.root.title('BananaOS')
+        self.root.title('Banana0S')
 
-        self.bg = bg
+        self.color = color
         self.height = height
         self.width = width
 
-        # self.blankscreen = Canvas(root, width=width*50, height=500, bg=bg)
-        # self.blankscreen.grid(row=0, column=0)
+        self.menu = Menu(root) #make the upper menu
 
-        self.menu = Menu(root)
-
-        self.computer_menu = Menu(self.menu, tearoff=0)
+        self.computer_menu = Menu(self.menu, tearoff=0) # create the computer menu
         self.computer_menu.add_command(label="About", command=self.about)
         self.computer_menu.add_command(label="Help", command=self.help)
-
         self.computer_menu.add_separator()
+        self.computer_menu.add_command(label="Quit", command=self.quit)
 
-        self.computer_menu.add_command(label="Quit", command=root.quit)
+        self.menu.add_cascade(label="BananaOS", menu=self.computer_menu) # add the computer menu to the upper menu
 
-        self.menu.add_cascade(label="BananaOS", menu=self.computer_menu)
+        root.config(menu=self.menu) # initialize the upper menu
 
-        root.config(menu=self.menu)
+        self.app_font = font.Font(family='Helvetica', size=17)
 
-        self.pinned_apps = {'BananaText': [self.start_BananaText], 'Games': [self.start_games],
-                            'Shell': [self.start_shell]}
-        self.apps = {'BananaText': [self.start_BananaText], 'Games': [self.start_games], 'Shell': [self.start_shell]}
-        self.appbar = []
 
-        self.current_app = None
+        self.pinned_apps = {'🌐':[self.start_BananaWeb], '📝': [self.start_BananaText], '🕹️': [self.start_Games], '👨‍💻': [self.start_Shell]}
+        self.apps = {'🌐':[self.start_BananaWeb], '📝': [self.start_BananaText], '🕹️': [self.start_Games], '👨‍💻': [self.start_Shell]}
 
-        self.draw_app_bar()
+        self.draw_appbar()
+        self.draw_screen()
 
-    def draw_app_bar(self):
-        x = 0
+    def draw_appbar(self):
+        """Draws the appbar
+        """
+        apps_drawn = 0
         self.appbar = []
 
         for appname, app in self.apps.items():
-            self.appbar.append(Button(command=app[0], text=appname))
-            self.appbar[x].grid(row=1, column=x + 1)
-            x += 1
+            self.appbar.append(Button(command=app[0], text=appname, font=self.app_font))
+            self.appbar[apps_drawn].grid(row=1, column=apps_drawn + 1)
+            apps_drawn += 1
 
-    def close_app_bar(self):
-        x = 0
+    def close_app(self, app):
+        app.grid_remove()
+        self.draw_screen()
 
-        for appname, app in self.apps.items():
-            self.appbar[x].grid_remove()
-            x += 1
-
-
-    def start_BananaText(self):
-        # close the current program (if there is one)
-        self.program_close()
-
-        # remove the blank screen (currently not working)
-        # self.blankscreen.grid_remove()
-
-        # create the bananatext app and put it on the screen
-        app = BananaText(self.root, self)
-        app.grid(row=0, column=0)
-        # change the current app.
-        self.current_app = app
-
-    def start_games(self):
-        # close the current program (if there is one)
-        self.program_close()
-
-        # remove the blank screen (currently not working)
-        # self.blankscreen.grid_remove()
-
-        # create the bananatext app and put it on the screen
-        app = Games(self.root, self)
-        app.grid(row=0, column=0)
-        # change the current app.
-        self.current_app = app
-
-    def start_sudoku(self):
-
-        # close the current program (if there is one)
-        self.program_close()
-
-
-        # create the sudoku app and put it on the screen
-        app = SudokuGrid(self.root)
-        app.grid(row=0, column=0)
-        # change the current app.
-        self.current_app = app
-
-    def start_shell(self):
-        # close the current program (if there is one)
-        self.program_close()
-
-        # remove the blank screen (currently not working)
-        # self.blankscreen.grid_remove()
-
-        # create the sudoku app and put it on the screen
-        app = Shell(self.root, self)
-        app.grid(row=0, column=0)
-        # change the current app.
-        self.current_app = app
-
-    def start_shotput(self):
-        # close the current program (if there is one)
-        self.program_close()
-
-
-
-        # create the sudoku app and put it on the screen
-        app = Shotput(self.root, self)
-        app.grid(row=0, column=0)
-        # change the current app.
-        self.current_app = app
-
-    def program_close(self):
-        self.close_app_bar()
-        self.apps = self.pinned_apps
-        self.draw_app_bar()
-        self.apps = self.pinned_apps
-        if self.current_app == None:
-            return
-        self.current_app.close()
-
-        current_app = None
+    def draw_screen(self):
+        """Draws blank screen
+        """
+        self.app = Screen(self.root, self)
+        self.app.grid(row=0, column=0)
+    
+    def close_screen(self):
+        """Closes the blank screen. Run this before you first in the start_<app> function. """
+    
 
     def about(self):
-        messagebox.showinfo("About", "Created in 2020 by Isaiah Day, Isaiah Firestone and Mila Day.")
+        messagebox.showinfo("About", "Created in 2020-2021 by Isaiah Day.\nGithub: Isaiah08-D")
 
     def help(self):
         messagebox.showinfo("Help",
-                            "Genral Help:\n Genral help coming soon.\nTo get more help, open the shell and type help.")
+                            "Genral Help:\n Genral help coming soon.\nTo get more help, open the shell and type help.")    
+    
+    
+    def start_BananaText(self):
+        self.close_screen() # close the blank screen
+
+        self.current_app = BananaText(self.master, self) # run the bananatext app
+        self.current_app.grid(row=0, column=0, rowspan=self.width)
+        
+    def start_Games(self): pass
+
+    def start_Shell(self): pass
+
+    def start_BananaWeb(self): pass
 
 
 root = Tk()
-main = Main(root, height=10, width=10, bg='yellow')
+main = Main(root, height=10, width=10, color='yellow')
 root.mainloop()
